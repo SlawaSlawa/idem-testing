ff6af828945509a22a562595c33c14d83eea777d
SlawaSlawa <slawapocht4@yandex.ru>
2023-04-25 22:24:52 +0400

    added breakpoints for slider

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -6,5 +6,18 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.feature__btns-item--next',
         prevEl: '.feature__btns-item--prev',
     },
-
+    breakpoints: {
+        280: {
+            slidesPerView: 1.2,
+            spaceBetween: 45,
+        },
+        768: {
+            slidesPerView: 2.2,
+            spaceBetween: 30,
+        },
+        1440: {
+            slidesPerView: 3.7,
+            spaceBetween: 121,
+        }
+    }
 });
